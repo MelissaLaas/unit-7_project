4f5cc7cd3b8c13e7b9bf9d60af659625bc9a26f8
MelissaLaas <melissa.laas16@gmail.com>
2021-06-28 14:18:53 +0400

    add charts code

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,4 @@
+// variables
 const alertBanner = document.getElementById("alert");
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
@@ -5,14 +6,12 @@ const send = document.getElementById("send");
 
 // alert banner - html
 alertBanner.innerHTML =
-`
-<div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+`<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
     <p class="alert-banner-close">x</p>
 </div>
 `
-    // alert banner - click 
+// alert banner - click 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
         if (element.classList.contains("alert-banner-close")) {
@@ -20,16 +19,48 @@ alertBanner.addEventListener('click', e => {
         }  
 }); 
 
-    //messaging section
-send.addEventListener('click', () => {
+//messaging section
+form.addEventListener('submit', e => {
     // check if user and message fields are empty
     if (user.value === "" && message.value === "") {
         alert("Please fill out user and message fields before sending");
+        e.preventDefault();
     } else if (user.value === "" ) {
         alert("Please fill out user field before sending");
+        e.preventDefault();
     } else if (message.value === "" ) {
         alert("Please fill out message field before sending");
+        e.preventDefault();
     } else {
         alert(`Message successfully sent to: ${user.value}`);
     }
 });
+
+//traffic
+trafficNav.addEventListener('click', e =>{
+    const element = e.target;  
+    const datatype = element.textContent;   
+
+    if(element.className ===  "traffic-nav-link" || element.className ===  "traffic-nav-link active"){
+        //change active button to inactive
+        let lis = document.querySelectorAll(".traffic-nav-link");
+        for(let i=0; i<lis.length; i++){
+            let activeButton = lis[i];
+            if(activeButton.className==="traffic-nav-link active"){
+                activeButton.className="traffic-nav-link"
+            }
+        }
+        //change active button to active
+        if(element.className==="traffic-nav-link"){
+            element.className = "traffic-nav-link active";
+        }
+        
+        //change graph
+            updateTrafficData(datatype);
+            updateChart(trafficChart, trafficData);
+        }
+    }  
+);
+
+
+///// localStorage //////
--- js/charts.js
+++ js/charts.js
@@ -1,10 +1,18 @@
-//LINE GRAPH
+let trafficCanvas = document.getElementById('traffic-chart');
+
+//LINE GRAPH - traffic
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
         backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderColor: 'rgba(112, 127, 194, .3)',
         borderWidth: 1,
+        fill: {
+            target: 'origin',
+            below: 'rgba(112, 127, 194, .3)'
+        },
+        clip: 0
     }]
 };
 
@@ -25,16 +33,15 @@ let trafficOptions = {
     }
 };
 
-
+//create first chart
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
 });
 
-//BAR GRAPH
+//BAR GRAPH - daily
 const dailyCanvas = document.getElementById("daily-chart");
-
 const dailyData = {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
     datasets: [{
@@ -43,7 +50,8 @@ const dailyData = {
         backgroundColor: '#7477BF',
         borderWidth: 1
         }]
-    };
+};
+
 const dailyOptions = {
     scales: {
         y: {
@@ -63,25 +71,21 @@ let dailyChart = new Chart(dailyCanvas, {
     options: dailyOptions
 });
 
-//DONUT GRAPH
+//DONUT GRAPH - mobile
 const mobileCanvas = document.getElementById("mobile-chart");
-//object literal for data
 const mobileData = {
     labels: ["Desktop", "Tablet", "Phones"],
     datasets: [{
         label: '# of Users',
         data: [2000, 550, 500],
         borderWidth: 0,
-        backgroundColor: [
-            '#7477BF',
-            '#78CF82',
-            '#51B6C8'
-        ]
+        backgroundColor: ['#7477BF', '#78CF82', '#51B6C8']
     }]
 };
 
 //object literal for options
 const mobileOptions = {
+    aspectRatio: 2,
     plugins: {
         legend: {
             position: 'right',
